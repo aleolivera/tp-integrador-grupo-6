fd4cb9a6c27e6cfa38f387a92cbd61e8e0fb7f0e
matias <55811800+LedesmaMatias@users.noreply.github.com>
2022-11-12 06:40:21 -0300

    mis necesidades

---
+++ app/src/main/java/com/example/comedores/adapters/ListViewMisNecesidadesAdapter.java
@@ -0,0 +1,54 @@
+package com.example.comedores.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.comedores.R;
+import com.example.comedores.entidades.Necesidad;
+
+import java.util.List;
+
+public class ListViewMisNecesidadesAdapter extends ArrayAdapter<Necesidad>  {
+
+    private List<Necesidad> listaNecesidades;
+    private Context context;
+    private int resourseLayout;
+    Button btnModificarEstado;
+
+    public ListViewMisNecesidadesAdapter(@NonNull Context context, int resource, @NonNull List<Necesidad> listaNecesidades) {
+        super(context, resource, listaNecesidades);
+        this.listaNecesidades=listaNecesidades;
+        this.context=context;
+        this.resourseLayout=resource;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view= convertView;
+        if(view==null)
+            view= LayoutInflater.from(context).inflate(resourseLayout,null);
+
+        Necesidad n= listaNecesidades.get(position);
+        TextView tvDescripcion= (TextView)view.findViewById(R.id.tvNecesidadDescripcion);
+        TextView tvTipo=(TextView) view.findViewById(R.id.tvNecesidadTipo);
+        TextView tvPrioridad= (TextView)view.findViewById(R.id.tvNecesidadPrioridad);
+        TextView tvEstado= (TextView)view.findViewById(R.id.tvNecesidadEstado);
+
+        tvDescripcion.setText("Descripcion: "+String.valueOf(n.getDescripcion()));
+        tvTipo.setText("Tipo: "+n.getTipo());
+        tvPrioridad.setText("Prioridad: "+n.getPrioridad());
+        tvEstado.setText("Estado: "+n.getEstado().getDescripcion());
+
+        return view;
+    }
+
+
+}
--- app/src/main/java/com/example/comedores/ui/misNecesidades/MisNecesidadesFragment.java
+++ app/src/main/java/com/example/comedores/ui/misNecesidades/MisNecesidadesFragment.java
@@ -5,15 +5,31 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.comedores.R;
+import com.example.comedores.adapters.ListViewMisNecesidadesAdapter;
+import com.example.comedores.conexion.DataComedores;
+import com.example.comedores.conexion.DataNecesidades;
+import com.example.comedores.entidades.Necesidad;
+import com.example.comedores.entidades.Usuario;
+import com.example.comedores.viewmodels.UsuarioViewModel;
+
+import java.util.List;
 
 public class MisNecesidadesFragment extends Fragment {
+
+    private UsuarioViewModel viewModel;
+    private ListView lvMisNecesidades;
+    private List<Necesidad> listaNecesidades;
+    private Usuario usuario;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +41,40 @@ public class MisNecesidadesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        obtenerUsuario();
+        cargarUI(view);
+        iniciarListView();
+    }
 
-        Toast.makeText(getContext(), "Fragment Mis necesidades", Toast.LENGTH_SHORT).show();
+    private void iniciarListView() {
+        cargarListView();
+        lvMisNecesidades.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Necesidad n = (Necesidad)lvMisNecesidades.getItemAtPosition(i);
+                DataNecesidades task = new DataNecesidades(getContext(), n);
+                task.execute("modificarNecesidad");
+                cargarListView();
+            }
+        });
+
+    }
+
+    private void cargarListView() {
+        DataComedores task = new DataComedores(getContext(), usuario.getComedor(), lvMisNecesidades, listaNecesidades);
+        task.execute("CargarNecesidades");
+    }
+
+    private void obtenerUsuario() {
+        viewModel= new ViewModelProvider(requireActivity()).get(UsuarioViewModel.class);
+        usuario=viewModel.getData().getValue();
+    }
+
+    private void cargarUI(View view) {
+        lvMisNecesidades = (ListView) view.findViewById(R.id.lvMisNecesidades);
+    }
+
+    private void cambiarEstado() {
+
     }
 }
