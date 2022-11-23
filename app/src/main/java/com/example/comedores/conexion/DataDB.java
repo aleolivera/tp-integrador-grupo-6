69ee03374553b4c00b14f83b1bf2f62adfaa4345
aleolivera <aleoliverarp10@gmail.com>
2022-11-23 18:41:57 -0300

    actualizo las constantes de conexion

--- app/src/main/java/com/example/comedores/conexion/DataDB.java
+++ app/src/main/java/com/example/comedores/conexion/DataDB.java
@@ -4,9 +4,9 @@ public class DataDB {
 
     public final static String HOST = "sql10.freesqldatabase.com";
     public final static String PORT = "3306";
-    public final static String NAMEDB = "sql10559525";
-    public final static String USER = "sql10559525";
-    public final static String PASS = "m9N5QbaW2z";
+    public final static String NAMEDB = "sql10579064";
+    public final static String USER = "sql10579064";
+    public final static String PASS = "lHJvAtLX9k";
 
     public static String URLMYSQL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + NAMEDB;
     public static String DRIVER = "com.mysql.jdbc.Driver";
