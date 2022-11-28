1d924f47ac4f836b9536fb914bb3cc755617221b
aleolivera <aleoliverarp10@gmail.com>
2022-11-28 18:58:29 -0300

    cambie las constantes para la nueva base de datos

--- app/src/main/java/com/example/comedores/conexion/DataDB.java
+++ app/src/main/java/com/example/comedores/conexion/DataDB.java
@@ -4,9 +4,9 @@ public class DataDB {
 
     public final static String HOST = "sql10.freesqldatabase.com";
     public final static String PORT = "3306";
-    public final static String NAMEDB = "sql10579064";
-    public final static String USER = "sql10579064";
-    public final static String PASS = "lHJvAtLX9k";
+    public final static String NAMEDB = "sql10581575";
+    public final static String USER = "sql10581575";
+    public final static String PASS = "fxwIggPuvH";
 
     public static String URLMYSQL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + NAMEDB;
     public static String DRIVER = "com.mysql.jdbc.Driver";
